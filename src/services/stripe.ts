f47b73759cef1a4b6c82aaf928f4110cc15f2aa3
Yara Polana <yara.polana@gmail.com>
2021-04-17 18:52:51 +0000

    add stripe

---
+++ src/services/stripe.ts
@@ -0,0 +1,12 @@
+import Stripe from 'stripe'
+import { version } from '../../package.json'
+
+export const stripe = new Stripe(
+  process.env.STRIPE_SECRET_KEY, {
+    apiVersion: '2020-08-27',
+    appInfo: {
+      name: 'IG News',
+      version,
+    }
+  })
+  
