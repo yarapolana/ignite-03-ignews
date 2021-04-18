27f224adadf38ec848325a78fe73761204809ec1
Yara Polana <yara.polana@gmail.com>
2021-04-18 22:11:55 +0000

    finalize subscribe button

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,12 +1,43 @@
+import { useSession, signIn } from 'next-auth/client'
+import { useRouter } from 'next/router'
+import { api } from '../../services/api'
+import { getStripeClient } from '../../services/stripe-client'
 import styles from './styles.module.scss'
 
-interface SubscribeButtonProps {
-  priceId: string
-}
+export function SubscribeButton() {
+  const [session] = useSession()
+  const router = useRouter()
 
-export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  async function handleSubscribe() {
+    if (!session) {
+      signIn('github')
+    }
+
+    // @ts-ignore
+    if (session.activeSubscription) {
+      router.push('/posts')
+      return
+    }
+
+    try {
+      const {
+        data: { sessionId }
+      } = await api.post<{ sessionId: string }>('/subscribe')
+
+      const stripe = await getStripeClient()
+
+      await stripe.redirectToCheckout({ sessionId })
+    } catch (err) {
+      alert(err.message)
+    }
+  }
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      onClick={handleSubscribe}
+      className={styles.subscribeButton}
+    >
       Subscribe
     </button>
   )
