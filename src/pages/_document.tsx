32ef93e34c676bb484163e2db1876ded7effd487
Yara Polana <yara.polana@gmail.com>
2021-04-18 22:13:06 +0000

    lint

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,15 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-export default class MyDocument extends Document{
+export default class MyDocument extends Document {
   render() {
-    return(
+    return (
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap"
+            rel="stylesheet"
+          />
           <link rel="icon" href="/favicon.png" />
         </Head>
         <body>
