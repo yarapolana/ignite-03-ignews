0abfc566ed65b5620e8510737b09bf04d01b4d04
Yara Polana <yara.polana@gmail.com>
2021-04-18 22:13:17 +0000

    add auth provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
+import { Provider as NextAuthProvider } from 'next-auth/client'
+
 import { Header } from '../components/Header'
 import '../styles/global.scss'
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <NextAuthProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </NextAuthProvider>
   )
 }
