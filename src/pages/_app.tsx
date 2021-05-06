bf79f88d265873d9d5b00a0bbafea20d0f2e6ca2
Yara Polana <yara.polana@gmail.com>
2021-05-06 15:25:08 +0000

    add typing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,10 @@
+import { AppProps } from 'next/app'
 import { Provider as NextAuthProvider } from 'next-auth/client'
 
 import { Header } from '../components/Header'
 import '../styles/global.scss'
 
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <NextAuthProvider session={pageProps.session}>
       <Header />
